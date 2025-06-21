47d4e6bf874c1adbd070372c2f2d80271165b92f
Mehak Narang <139652649+mehak-World@users.noreply.github.com>
2025-06-21 13:35:25 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,30 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "simple-node-app"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install docker -y",
+      "sudo service docker start",
+      "sudo usermod -a -G docker ec2-user"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+}
 
